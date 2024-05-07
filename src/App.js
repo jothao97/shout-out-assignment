09d9cfdfb8d9e60a2197cf57f54824441262732d
JoAnn <joann.thao@live.com>
2024-05-07 15:21:27 -0500

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import datePicker from "react-datepicker"
 
 function App() {
+
+  const [startDate, setStartDate] = useState();
+
+  const [announcementMessage, setAnnouncementMessage] = useState('');
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {announcementMessage}
+      <input name='users name' type='text'></input>
+      <datePicker selected={startDate} onChange={(date) => setStartDate(date)} />
     </div>
   );
 }
